1e76720761b01e7f1f08cb3e3aa5ae1a09816275
arashakb <arashakbari1234@gmail.com>
2025-11-07 10:59:57 -0800

    fixed the scripts

--- libero_evaluation.sh
+++ libero_evaluation.sh
@@ -10,19 +10,14 @@ echo "Started: $(date)"
 echo "=========================================="
 
 # Environment setup
-export NCCL_P2P_DISABLE=1
-export WANDB_API_KEY="e5cbd387d8e0c181e93c7e4ec56e965c5115e94c"
-export WANDB_CONFIG_DIR="/home/user1/.config/wandb_arash"
-export WANDB_CACHE_DIR="/home/user1/.cache/wandb_arash"
-export PRISMATIC_DATA_ROOT=/home/user1/aras_prism_training/OXE/
-export PYTHONPATH=/home/user1/arashwork/moxinvla-oft
-export CUDA_VISIBLE_DEVICES="3"
-export PYTHONPATH=/home/user1/arashwork/moxinvla-oft/LIBERO:$PYTHONPATH
+
+export CUDA_VISIBLE_DEVICES="0"
+# export PYTHONPATH=/home/user1/arashwork/moxinvla-oft/LIBERO:$PYTHONPATH
 
 # Run evaluation
 python experiments/robot/libero/run_libero_eval.py \
   --model_family openvla \
-  --pretrained_checkpoint ./logs/libero_finetune_logs_checkpoints/moxin-7b-oxe-250k-image_aug-libero-object_no_film_proprio_1_img_3e-4/moxin-7b-224px+mx-oxe-magic-soup+n1+b32+x7-250k--image_aug+libero_object_no_noops+b16+lr-0.0003+lora-r32--2_imgs--proprio--90000_chkpt/ \
+  --pretrained_checkpoint "YOUR PRETRAINED CHECKPOINT" \
   --task_suite_name libero_object \
   --num_images_in_input 2 \
   --use_film False \
--- libero_finetune.sh
+++ libero_finetune.sh
@@ -10,21 +10,19 @@ echo "Started: $(date)"
 echo "=========================================="
 
 # Environment setup
-export NCCL_P2P_DISABLE=1
-export WANDB_API_KEY="e5cbd387d8e0c181e93c7e4ec56e965c5115e94c"
-export WANDB_CONFIG_DIR="/home/user1/.config/wandb_arash"
-export WANDB_CACHE_DIR="/home/user1/.cache/wandb_arash"
-export PYTHONPATH=/home/user1/arashwork/moxinvla-oft
+# export NCCL_P2P_DISABLE=1
+export WANDB_API_KEY="YOUR WANDB API KEY"
+# export PYTHONPATH="YOUR PYTHONPATH"
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 
 # Run training
 torchrun --standalone --nnodes 1 --nproc-per-node 8 vla-scripts/finetune.py \
-  --vla_path moxin-hf-convert \
+  --vla_path "YOUR VLA PATH (HF Converted Pretrained checkpoint)" \
   --data_root_dir modified_libero_rlds_data \
   --dataset_name libero_spatial_no_noops \
-  --run_root_dir logs/checkpoints/moxin-libero-spatial-2img-3e-4 \
+  --run_root_dir "YOUR LOG AND CHECKPOINT DIR" \
   --lora_rank 32 \
-  --batch_size 1 \
+  --batch_size 8 \
   --grad_accumulation_steps 1 \
   --learning_rate 3e-4 \
   --use_film False \
@@ -33,8 +31,8 @@ torchrun --standalone --nnodes 1 --nproc-per-node 8 vla-scripts/finetune.py \
   --resume False \
   --image_aug True \
   --save_latest_checkpoint_only False \
-  --wandb_project OpenVLA-OFT-Moxin_7B_VLM_finetune_LIBERO_SPATIAL_8H100_testtt \
-  --wandb_entity arash-akbari-stu-northeastern-university \
+  --wandb_project "YOUR WANDB ENTITY" \
+  --wandb_entity "YOUR WANDB PROJECT" \
   --save_freq 10000 \
   --max_steps 50000 2>&1 | tee "$LOG_FILE"
 
